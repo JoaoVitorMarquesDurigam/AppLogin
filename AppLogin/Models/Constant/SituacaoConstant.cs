3b54cb97f4d25519e25f28894ddfa69ddcd5b92d
João vitor marques durigam <joaovitordurigam@gmail.com>
2026-05-12 09:23:12 -0300

    classes

---
+++ AppLogin/Models/Constant/SituacaoConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppLogin.Models.Constant
+{
+    public class SituacaoConstant
+    {
+        public const string Ativado = "A";
+        public const string Desativado = "D";
+    }
+}
--- AppLogin/obj/Debug/net10.0/AppLogin.AssemblyInfo.cs
+++ AppLogin/obj/Debug/net10.0/AppLogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df30ab9c9ca5d4078c906158ae2c60c773cc7664")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
