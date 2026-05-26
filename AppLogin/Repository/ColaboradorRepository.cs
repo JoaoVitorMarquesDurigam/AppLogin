d0b27cdcfc1dfc5e1e334e97571dc2b9809f2ffb
João vitor marques durigam <joaovitordurigam@gmail.com>
2026-05-26 09:19:17 -0300

    colaboradorrepositorio

--- AppLogin/Repository/ColaboradorRepository.cs
+++ AppLogin/Repository/ColaboradorRepository.cs
@@ -4,15 +4,19 @@ using AppLogin.Repository.Contract;
 using MySql.Data.MySqlClient;
 using System.Data;
 using X.PagedList;
+using X.PagedList.Extensions;
+
 
 namespace AppLogin.Repository
 {
     public class ColaboradorRepository : IColaboradorRepository
     {
         private readonly string _conexaoMySQL;
+        private readonly IConfiguration _conf;
         public ColaboradorRepository(IConfiguration conf)
         {
             _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");
+            _conf = _conf;
         }
 
         public Colaborador Login(string Email, string Senha)
@@ -73,9 +77,84 @@ namespace AppLogin.Repository
             }
         }
 
+        public IEnumerable<Colaborador> ObterTodosColaboradores()
+        {
+            List<Colaborador> colabList = new List<Colaborador>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM COLABORADOR", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    colabList.Add(
+                        new Colaborador
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = (string)(dr["Nome"]),
+                            Email = (string)(dr["Email"]),
+                            Senha = (string)(dr["Senha"]),
+                            Tipo = (string)(dr["Tipo"])
+                        });
+                }
+                return colabList;
+            }
+        }
+
+        public Colaborador ObterColaborador(int Id)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Colaborador WHERE Id=@Id ", conexao);
+                cmd.Parameters.AddWithValue("@Id", Id);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Colaborador colaborador = new Colaborador();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dr.Read())
+                {
+                    colaborador.Id = (Int32)(dr["Id"]);
+                    colaborador.Nome = (string)(dr["Nome"]);
+                    colaborador.Email = (string)(dr["Email"]);
+                    colaborador.Senha = (string)(dr["Senha"]);
+                    colaborador.Tipo = (string)(dr["Tipo"]);
+                }
+                return colaborador;
+            }
+        }
+
         public void Atualizar(Colaborador colaborador)
         {
-            throw new NotImplementedException();
+            string Tipo = ColaboradorTipoConstant.Comum;
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand(
+                    "update Colaborador set Nome=@Nome, " +
+                    " Email=@Email, Senha=@Senha, Tipo=@Tipo where Id=@Id ",
+                    conexao);
+
+                cmd.Parameters.Add("@Id", MySqlDbType.VarChar).Value = colaborador.Id;
+                cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = colaborador.Nome;
+                cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = colaborador.Email;
+                cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = colaborador.Senha;
+                cmd.Parameters.Add("@Tipo", MySqlDbType.VarChar).Value = Tipo;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
         }
 
         public void AtualizarSenha(Colaborador colaborador)
@@ -88,24 +167,43 @@ namespace AppLogin.Repository
             throw new NotImplementedException();
         }
 
-        public Colaborador ObterColaborador(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
         public List<Colaborador> ObterColaboradorPorEmail(string email)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Colaborador> ObterTodosColaboradores()
         {
             throw new NotImplementedException();
         }
 
         public IPagedList<Colaborador> ObterTodosColaboradores(int? pagina)
         {
-            throw new NotImplementedException();
+            int RegistroPorPagina = _conf.GetValue<int>("RegistroPorPagina");
+
+            int NumeroPagina = pagina ?? 1;
+            List<Colaborador> ListCat = new List<Colaborador>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from colaborador;", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    ListCat.Add(
+                        new Colaborador
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = (string)(dr["Nome"]),
+                            Senha = (string)(dr["Senha"]),
+                            Email = (string)(dr["Email"]),
+                            Tipo = (string)(dr["Senha"])
+
+                        });
+                }
+                return ListCat.ToPagedList<Colaborador>(NumeroPagina, RegistroPorPagina);
+            }
         }
     }
 }
